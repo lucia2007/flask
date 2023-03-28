074697460e45697f550f69ef40a94490aec3b226
Lucia Ferencik <lferencikova@gmail.com>
2023-03-28 11:29:16 +0000

    retrieve data from the form using request library

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -31,8 +31,11 @@ def about_member(member_name):
     # return "<h1>" + member["name"] + "</h1>"
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
